f7023f2729cc759f4e1d734549b18575e2b5e1fa
Andrii Pyrih <andrii.pyrih@gmail.com>
2023-06-20 23:33:06 +0100

    Create initial project structure

---
+++ station/station.ino
@@ -0,0 +1,12 @@
+#include "secrets.h"
+
+const char ssid[] = SECRET_SSID;
+const char pass[] = SECRET_PASS;
+
+void setup() {
+  // put your setup code here, to run once
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+}
